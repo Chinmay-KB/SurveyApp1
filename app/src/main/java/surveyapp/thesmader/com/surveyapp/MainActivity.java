8b1a8bb31cb2a2f73568bc48c8d50cb2778900b3
Thesmader <34758667+Thesmader@users.noreply.github.com>
2018-05-05 01:55:24 +0530

    Added nav drawer and Sign In

--- app/src/main/java/surveyapp/thesmader/com/surveyapp/MainActivity.java
+++ app/src/main/java/surveyapp/thesmader/com/surveyapp/MainActivity.java
@@ -3,6 +3,7 @@ package surveyapp.thesmader.com.surveyapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -11,10 +12,14 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
     public void dataEntry(View view)
     {
